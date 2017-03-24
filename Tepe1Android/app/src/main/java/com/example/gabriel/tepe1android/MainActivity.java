86f3e830293880c4376f060ac6fb6c8ec47059c9
Hernan Brunstein <hernanb2000@gmail.com>
2017-03-24 14:46:35 -0300

    Termine el punto 1c y cambie la variable "primertexto" por "primerTexto"

--- Tepe1Android/app/src/main/java/com/example/gabriel/tepe1android/MainActivity.java
+++ Tepe1Android/app/src/main/java/com/example/gabriel/tepe1android/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.gabriel.tepe1android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
     public void presionarBoton(View vista) {
         //Recibo los Editetxts y guardo sus valores en varibales string
         EditText primerEdit = (EditText)findViewById(R.id.primerEditText);
-        String primertexto = primerEdit.getText().toString();
+        String primerTexto = primerEdit.getText().toString();
 
         EditText segundoEdit = (EditText)findViewById(R.id.segundoEditText);
         String segundoTexto = segundoEdit.getText().toString();
 
         //Recibo el largo de los dos strings
-        int largoPrimerTexto = primertexto.length();
+        int largoPrimerTexto = primerTexto.length();
         int largoSegundoTexto = segundoTexto.length();
 
         //Muestro los largos en un toast
@@ -34,10 +37,20 @@ public class MainActivity extends AppCompatActivity {
         if (largoPrimerTexto==largoSegundoTexto) {
             Toast.makeText(this,"Los dos textos tienen el mismo largo",Toast.LENGTH_SHORT).show();
         }
-        //Si no tienen el mismo largo resto el primero por el segundo y muestro el valor absoluto
+        //Si no tienen el mismo largo, resto el primero por el segundo y muestro el valor absoluto
         else{
             int diferenciaEntreTextos = Math.abs(largoPrimerTexto-largoSegundoTexto);
             Toast.makeText(this,"El texto mas grande tiene " +diferenciaEntreTextos+ " caracteres mas que el corto", Toast.LENGTH_SHORT).show();
         }
+
+        //Saco los espacios del principio y del final
+        String primerTextoTrimeado=primerTexto.trim();
+        String segundoTextoTrimeado=segundoTexto.trim();
+
+        //Declaro e instancio una variable con los primeros 3 caracteres de cada texto concatenados
+        String caracteresConcatenados = primerTextoTrimeado.substring(0,3)+segundoTextoTrimeado.substring(0,3);
+
+        //Muestro en un Toast caracteresConcatenados
+        Toast.makeText(this,"Los primeros 3 caracteres de cada texto concatenados son: "+caracteresConcatenados,Toast.LENGTH_SHORT).show();
     }
 }
