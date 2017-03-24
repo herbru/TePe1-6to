4614d8050fd4219d3c5d0c8478fdb3003ab0052f
Hernan Brunstein <hernanb2000@gmail.com>
2017-03-24 13:18:15 -0300

    termine el punto 1a

--- Tepe1Android/app/src/main/java/com/example/gabriel/tepe1android/MainActivity.java
+++ Tepe1Android/app/src/main/java/com/example/gabriel/tepe1android/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.gabriel.tepe1android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    //Metodo que se ejecuta al presionar el boton "Ingresar"
+    public void presionarBoton(View vista)
+    {
+        //Recibo los Editetxts y guardo sus valores en varibales string
+        EditText primerEdit = (EditText)findViewById(R.id.primerEditText);
+        String primertexto = primerEdit.getText().toString();
+
+        EditText segundoEdit = (EditText)findViewById(R.id.segundoEditText);
+        String segundoTexto = segundoEdit.getText().toString();
+
+        //Recibo el largo de los dos strings
+        int largoPrimerTexto = primertexto.length();
+        int largoSegundoTexto = segundoTexto.length();
+
+        //Muestro los largos en un toast
+        Toast.makeText(this, "El 1er texto tiene "+largoPrimerTexto+" caracteres y el 2do tiene "+largoSegundoTexto+" caracteres", Toast.LENGTH_SHORT).show();
     }
 }
